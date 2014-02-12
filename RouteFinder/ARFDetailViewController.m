6dd812a5bcfe06463dbc8e5b81506480a5ca7e91
Denis C de Azevedo <denis.azevedo@gmail.com>
2014-02-12 01:07:49 -0200

    code cleanup

--- RouteFinder/ARFDetailViewController.m
+++ RouteFinder/ARFDetailViewController.m
@@ -28,8 +28,6 @@
 
 - (void)configureView
 {
-    // Update the user interface for the detail item.
-
     if (self.detailItem) {
         self.detailDescriptionLabel.text = [self.detailItem description];
     }
@@ -38,14 +36,7 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
-    [self configureView];
-}
-
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+	[self configureView];
 }
 
 @end
