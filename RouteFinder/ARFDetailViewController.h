7d7bb6d41a533b2a2552c42550abe206f75be447
Denis C de Azevedo <denis.azevedo@gmail.com>
2014-02-12 00:44:03 -0200

    create xcode master-view project

---
+++ RouteFinder/ARFDetailViewController.h
@@ -0,0 +1,16 @@
+//
+//  ARFDetailViewController.h
+//  RouteFinder
+//
+//  Created by Denis C de Azevedo on 12/02/14.
+//  Copyright (c) 2014 Denis C de Azevedo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ARFDetailViewController : UIViewController
+
+@property (strong, nonatomic) id detailItem;
+
+@property (weak, nonatomic) IBOutlet UILabel *detailDescriptionLabel;
+@end
---
+++ RouteFinder/ARFMasterViewController.h
@@ -0,0 +1,13 @@
+//
+//  ARFMasterViewController.h
+//  RouteFinder
+//
+//  Created by Denis C de Azevedo on 12/02/14.
+//  Copyright (c) 2014 Denis C de Azevedo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ARFMasterViewController : UITableViewController
+
+@end
